0ac0f1026453aa006e731e54022c10fbbccadb17
QAXCrat <lm16310@163.com>
2020-10-28 23:31:18 +0800

    加密IAT

--- Packer/header.h
+++ Packer/header.h
@@ -2,7 +2,7 @@
 #include <Windows.h>
 #include <stdio.h>
 #include <strsafe.h>
-
+#include <locale.h>
 
 typedef struct PESTRUCT
 {
@@ -10,11 +10,14 @@ typedef struct PESTRUCT
 
 	IMAGE_DATA_DIRECTORY ROC;
 
+	DWORD OEP;
+
+	DWORD oldImageBase;
+
 	PVOID mem_pe_base;
 
 	PIMAGE_OPTIONAL_HEADER m_dwpOptHeader;
 
-	DWORD OEP;
 
 }Pestruct;
 
@@ -31,44 +34,18 @@ typedef struct STRUCTUNPACKER
 
 Pestruct InitStuct(Pestruct m_pestruct);
 
-/**
-* @brief Need save to file data
-*/
-//strlen(compreed data)
-INT m_dwCompressedDataSize;
 
-
-
-/**
-* @brief class global variable
-*/
 BOOL IsDebug;
-DWORD m_dwOldEntryPoint;
-DWORD m_dwFileSize;
-LPCSTR m_pSouceFile;
-PVOID m_dwpPeBase;
-PIMAGE_DOS_HEADER m_dwpDosHeader;
-PIMAGE_NT_HEADERS m_dwpNTHeader;
-PIMAGE_FILE_HEADER m_dwpFileHeader;
-PIMAGE_OPTIONAL_HEADER m_dwpOptHeader;
-PIMAGE_SECTION_HEADER m_dwpLastSec;
-PVOID m_dwpNePeMem;
 
-//Points to the compressed code
-PVOID m_dwpComperedData;
-//Points to the Section headers 
-PVOID m_dwpSecheadersData;
+//LPWSTR DllPath = TEXT("C:\\Users\\Cray\\Desktop\\encrypting-shell\\Release\\Packdll.dll");
+LPWSTR DllPath = TEXT(".\\Packdll.dll");
 
-LPWSTR DllPath = TEXT("C:\\Users\\Cray\\Desktop\\Packer\\Release\\Packdll.dll");
-LPWSTR FilePath = TEXT("C:\\Users\\Cray\\Desktop\\Packer\\Release\\Mssb.exe");
+//LPWSTR FilePath = TEXT("C:\\Users\\Cray\\Desktop\\encrypting-shell\\Release\\demo.exe");
+wchar_t FilePath[MAX_PATH] = { 0, };
 
-/**
-* @brief Functions
-*/
 PVOID LoadFile2Mem(LPWSTR m_pSouceFile);
-BOOL Run(LPCSTR sourceFile);
-VOID GetError(LPCSTR lpszFunction);
-VOID EncryptExc(Pestruct);
+BOOL Run(LPWSTR sourceFile);
+VOID DecryptExc(Pestruct);
 VOID ClearI_AT_ROC(Pestruct);
 StructUnPacker LoadPackdll();
 VOID ConstructNewPE(Pestruct, StructUnPacker);
@@ -77,15 +54,9 @@ PIMAGE_DOS_HEADER GetDosHeader(PVOID m_dwpPeBase);
 PIMAGE_NT_HEADERS GetNTHeader(PVOID m_dwpPeBase);
 PIMAGE_FILE_HEADER GetFileHeader(PVOID m_dwpPeBase);
 PIMAGE_OPTIONAL_HEADER GetOptHeader(PVOID m_dwpPeBase);
-PIMAGE_SECTION_HEADER LastSecionheader(PVOID m_dwpPeBase);
 PIMAGE_SECTION_HEADER GetSecionheader(PVOID m_dwpPeBase);
-DWORD CalcCompreDataSize(PVOID m_dwpPeBase);
-PVOID CompreData(PVOID m_dwpPeBase, INT m_compre_data_size);
-VOID dprintf(LPCSTR pPrintf_mes);
-PVOID SaveSecheader(PVOID m_dwpPeBase);
-PVOID Copy2NewMem(PVOID m_dwpPeBase, INT m_dwCompressedDataSize, PVOID m_dwpComperedData, INT m_dwSizeOfDecMem, PVOID m_dwpDecMem);
+
 INT GetAlignment(INT Alignment, INT FixValue);
-PVOID UpdateHeader(PVOID m_dwpPeBase, INT m_firstsecsize);
 
 int __CRTDECL mprintf(
 	_In_z_ _Printf_format_string_ char const* const _Format,
--- Packer/packer.c
+++ Packer/packer.c
@@ -2,43 +2,23 @@
 
 
 
-/**
-* @brief GetLastError and printf chinese  info
-* @param lpszFunction hint
-*/
-void GetError(LPCSTR lpszFunction)
-{
-	LPVOID lpMsgBuf;
-	char Mes[1024] = { 0, };
-	LPVOID lpDisplayBuf;
-	DWORD dw = GetLastError();
 
-	FormatMessageA(
-		FORMAT_MESSAGE_ALLOCATE_BUFFER |
-		FORMAT_MESSAGE_FROM_SYSTEM |
-		FORMAT_MESSAGE_IGNORE_INSERTS,
-		NULL,
-		dw,
-		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-		(LPSTR)&lpMsgBuf,
-		0, NULL);
-	sprintf_s(Mes, 1024, "[!] %s failed with error 0x%x: %s\nProcess will Kill", lpszFunction, dw, lpMsgBuf);
-	mprintf(Mes);
-	getchar();
-	ExitProcess(dw);
-}
 
-/**
-* @brief Load soucer file to memory
-* @param m_pSouceFile soucefile path
-* @return new memory addr
-*/
+//************************************
+// 函数名称: LoadFile2Mem
+// 函数说明: 加载被加壳程序到内存，未展开
+// 函数参数: LPWSTR m_pSouceFile
+// 返 回 值: PVOID 保存被加壳的一段内存
+// 创 建 人: Cray
+// 创建日期: 2020/10/26
+//************************************
 PVOID LoadFile2Mem(LPWSTR m_pSouceFile)
 {
 	HANDLE hFile = CreateFile(m_pSouceFile, GENERIC_READ, NULL, NULL, OPEN_EXISTING, NULL, NULL);
 	if (hFile == INVALID_HANDLE_VALUE)
 	{
-		GetError("loadFileToMem_CrateFile!");
+		mprintf("loadFileToMem_CrateFile!");
+
 		return NULL;
 	}
 
@@ -55,189 +35,100 @@ PVOID LoadFile2Mem(LPWSTR m_pSouceFile)
 
 	return pPeBase;
 }
-/**
-* @brief GetDosHeader
-* @param m_dwpPeBase  pMemFile
-* @return DosHeader
-*/
+
+
+//************************************
+// 函数名称: GetDosHeader
+// 函数说明: 获得Dos头
+// 函数参数: PVOID m_dwpPeBase
+// 返 回 值: PIMAGE_DOS_HEADER
+// 创 建 人: Cray
+// 创建日期: 2020/10/26
+//************************************
 PIMAGE_DOS_HEADER GetDosHeader(PVOID m_dwpPeBase)
 {
 	return (PIMAGE_DOS_HEADER)m_dwpPeBase;
 }
-/**
-* @brief GetNTHeader
-* @param m_dwpPeBase pMemFile
-* @return Ntheader
-*/
+
+//************************************
+// 函数名称: GetNTHeader
+// 函数说明: 获得Nt头
+// 函数参数: PVOID m_dwpPeBase
+// 返 回 值: PIMAGE_NT_HEADERS
+// 创 建 人: Cray
+// 创建日期: 2020/10/26
+//************************************
 PIMAGE_NT_HEADERS GetNTHeader(PVOID m_dwpPeBase)
 {
 	return (PIMAGE_NT_HEADERS)((PBYTE)GetDosHeader(m_dwpPeBase)->e_lfanew + (DWORD)m_dwpPeBase);
 }
-/**
-* @brief GetFileHeader
-* @param m_dwpPeBase pMemFile
-* @return FileHeader
-*/
+
+
+//************************************
+// 函数名称: GetFileHeader
+// 函数说明: 获取文件头
+// 函数参数: PVOID m_dwpPeBase
+// 返 回 值: PIMAGE_FILE_HEADER
+// 创 建 人: Cray
+// 创建日期: 2020/10/26
+//************************************
 PIMAGE_FILE_HEADER GetFileHeader(PVOID m_dwpPeBase)
 {
 	return (PIMAGE_FILE_HEADER)((PBYTE)GetNTHeader(m_dwpPeBase) + 4);
 }
 
-/**
-* @brief GetOptHeader
-* @param m_dwpPeBase  pMemFile
-* @return OptHeader
-*/
+
+//************************************
+// 函数名称: GetOptHeader
+// 函数说明: 获得opt头
+// 函数参数: PVOID m_dwpPeBase
+// 返 回 值: PIMAGE_OPTIONAL_HEADER
+// 创 建 人: Cray
+// 创建日期: 2020/10/26
+//************************************
 PIMAGE_OPTIONAL_HEADER GetOptHeader(PVOID m_dwpPeBase)
 {
 	return (PIMAGE_OPTIONAL_HEADER)((PBYTE)GetFileHeader(m_dwpPeBase) + sizeof(IMAGE_FILE_HEADER));
 }
 
-/**
-* @brief GetlastSec
-* @param m_dwpPeBase  pMemFile
-* @return The PE File LastSecionheader
-*/
-PIMAGE_SECTION_HEADER LastSecionheader(PVOID m_dwpPeBase)
-{
-	return (PIMAGE_SECTION_HEADER)((PBYTE)(IMAGE_FIRST_SECTION(GetNTHeader(m_dwpPeBase))) + sizeof(IMAGE_SECTION_HEADER) * (GetFileHeader(m_dwpPeBase)->NumberOfSections - 1));
-}
 
-/**
-* @brief GetSecionheader
-* @param m_dwpPeBase  pMemFile
-* @return The PE File Secionheaders
-*/
+//************************************
+// 函数名称: GetSecionheader
+// 函数说明: 获得Sec头
+// 函数参数: PVOID m_dwpPeBase
+// 返 回 值: PIMAGE_SECTION_HEADER
+// 创 建 人: Cray
+// 创建日期: 2020/10/26
+//************************************
 PIMAGE_SECTION_HEADER GetSecionheader(PVOID m_dwpPeBase)
 {
 	return IMAGE_FIRST_SECTION(GetNTHeader(m_dwpPeBase));
 }
 
-/**
-* @brief calc File Size to exclude SizeOfHeaders, using to compression
-* @param m_dwpPeBase pMemFile
-* @return sizeofimage - SizeOfHeaders
-*/
-DWORD CalcCompreDataSize(PVOID m_dwpPeBase)
-{
-	PIMAGE_SECTION_HEADER pLastSec = LastSecionheader(m_dwpPeBase);
-	DWORD BinFileSize = pLastSec->PointerToRawData + pLastSec->SizeOfRawData;
-	return BinFileSize - GetOptHeader(m_dwpPeBase)->SizeOfHeaders;
-}
-
-/**
-* @brief compression data
-* @param m_dwpPeBase pMemFile
-* @param m_dwcompre_data_size  want to compre data size
-* @return
-*/
-PVOID CompreData(PVOID m_dwpPeBase, INT m_compre_data_size)
-{
-	INT m_max_dst_size = 0;
-
-	PCHAR compressed_data = (PCHAR)malloc((size_t)m_max_dst_size);
-	if (compressed_data == NULL)
-	{
-		GetError("CompreData_malloc");
-		return NULL;
-	}
-
-	PCHAR m_psouce_data = (PCHAR)m_dwpPeBase + GetOptHeader(m_dwpPeBase)->SizeOfHeaders;
-
-	m_dwCompressedDataSize = 0;
-
-	if (m_dwCompressedDataSize <= 0)
-	{
-		GetError("CompreData_LZ4_compress_default");
-		free(compressed_data);
-		return NULL;
-	}
-
-	char tmp_print_info[MAX_PATH];
-	FLOAT ratio = (FLOAT)m_dwCompressedDataSize / m_compre_data_size;
-	sprintf_s(tmp_print_info, MAX_PATH, "ratio : %.2f\n", ratio);
-
-	compressed_data = (PCHAR)realloc(compressed_data, m_dwCompressedDataSize);
-
-	if (compressed_data == NULL)
-	{
-		GetError("CompreData_realloc");
-		return NULL;
-	}
-
-	return compressed_data;
-}
-
-
-
-/**
-* @brief Save the section headers
-* @param m_dwpPeBase pPeBase
-* @return Section headers
-*/
-
-PVOID SaveSecheader(PVOID m_dwpPeBase)
-{
-	PVOID m_psec = (PVOID)IMAGE_FIRST_SECTION(GetNTHeader(m_dwpPeBase));
-
-	INT m_secSize = GetFileHeader(m_dwpPeBase)->NumberOfSections * sizeof(IMAGE_SECTION_HEADER);
-
-	PVOID m_pSecCopy = malloc(m_secSize);
-
-	if (m_pSecCopy == NULL)
-	{
-		GetError("SaveSecheader_malloc");
-		return NULL;
-	}
-
-	memcpy_s(m_pSecCopy, m_secSize, m_psec, m_secSize);
-
-	return m_pSecCopy;
-}
-
-/**
-* @brief Gets alignment values based on size
-* @param Alignment  FileAlignment or SectionAlignment
-* @param FixValue Need to fix value
-* @return fixed value
-*/
+//************************************
+// 函数名称: GetAlignment
+// 函数说明: 计算对齐，新加节的时候要算
+// 函数参数: INT Alignment  FileAlignment or SectionAlignmen
+// 函数参数: INT FixValue   Need to fix value
+// 返 回 值: INT
+// 创 建 人: Cray
+// 创建日期: 2020/10/26
+//************************************
 INT GetAlignment(INT Alignment, INT FixValue)
 {
 	return FixValue % Alignment == 0 ? FixValue : (FixValue / Alignment + 1) * Alignment;
 
 }
 
-/**
-* @brief 修改节个数 、待后续补充
-* @param m_dwpPeBase
-* @param m_firstsecsize
-* @return
-*/
-PVOID UpdateHeader(PVOID m_dwpPeBase, INT m_firstsecsize)
-{
-	INT m_sizeofheader = GetOptHeader(m_dwpPeBase)->SizeOfHeaders;
-	PVOID m_ppeheader = malloc(m_sizeofheader);
-	if (m_ppeheader == NULL)
-	{
-		GetError("m_dwpPeBase_malloc");
-		return NULL;
-	}
-	memcpy_s(m_ppeheader, m_sizeofheader, m_dwpPeBase, m_sizeofheader);
-
-	GetFileHeader(m_ppeheader)->NumberOfSections = 2;
-	PIMAGE_SECTION_HEADER m_psec = GetSecionheader(m_ppeheader);
-
-	*(m_psec->Name) = (char)"0Ops!";
-
-	m_psec->SizeOfRawData = m_firstsecsize;
-
-	*(m_psec[1].Name) = (char)"MyPack!";
-
-	return m_ppeheader;
-}
-
-//加密代码段，加密方式 Key = timestap^0x12344321
-VOID EncryptExc(Pestruct m_pestruct)
+//************************************
+// 函数名称: DecryptExc
+// 函数说明: 加密代码段，加密方式 Key = timestap^0x12344321
+// 函数参数: Pestruct m_pestruct
+// 返 回 值: VOID
+// 创 建 人: Cray
+// 创建日期: 2020/10/26
+//************************************
+VOID DecryptExc(Pestruct m_pestruct)
 {
 	DWORD start = 0;
 	DWORD end = 0;
@@ -247,7 +138,7 @@ VOID EncryptExc(Pestruct m_pestruct)
 
 	DWORD encryptKey =
 		GetFileHeader(m_pestruct.mem_pe_base)->TimeDateStamp;
-	
+
 	encryptKey = encryptKey ^ 0x12344321;
 
 	DWORD entrypoint =
@@ -256,18 +147,18 @@ VOID EncryptExc(Pestruct m_pestruct)
 	PIMAGE_SECTION_HEADER Psec = IMAGE_FIRST_SECTION(GetNTHeader(m_pestruct.mem_pe_base));
 	INT SecNumber = GetFileHeader(m_pestruct.mem_pe_base)->NumberOfSections;
 
-	for( ; SecNumber > 0  ; SecNumber--)
+	for (; SecNumber > 0; SecNumber--)
 	{
 		start = Psec->VirtualAddress;
 		end = Psec->VirtualAddress + Psec->SizeOfRawData;
 
-		if ( entrypoint > start &
-			entrypoint < end )
+		if (entrypoint >= start &
+			entrypoint <= end)
 		{
 			Encryptsize = Psec->SizeOfRawData;
 
-			FileStart = 
-			 (PCHAR)(m_pestruct.mem_pe_base) + Psec->PointerToRawData ;
+			FileStart =
+				(PCHAR)(m_pestruct.mem_pe_base) + Psec->PointerToRawData;
 
 			break;
 		}
@@ -276,22 +167,33 @@ VOID EncryptExc(Pestruct m_pestruct)
 
 	for (i = 0; 4 * i < Encryptsize;i++)
 	{
-		*FileStart = *FileStart ^ encryptKey;
+		if (*FileStart)
+		{
+			*FileStart = *FileStart ^ encryptKey;
 
-		encryptKey = encryptKey + *FileStart;
+			encryptKey = encryptKey << 25 | encryptKey >> 7;
+		}
 
 		FileStart++;
 
 	}
-	
+
 }
 
 
+//************************************
+// 函数名称: ClearI_AT_ROC
+// 函数说明: 把被加壳的原始IAT 和 ROC都给抹掉
+// 函数参数: Pestruct m_pestruct
+// 返 回 值: VOID
+// 创 建 人: Cray
+// 创建日期: 2020/10/26
+//************************************
 VOID ClearI_AT_ROC(Pestruct m_pestruct)
 {
 
-	m_pestruct.m_dwpOptHeader->DataDirectory[1].VirtualAddress = 0; 
-	m_pestruct.m_dwpOptHeader->DataDirectory[1].Size = 0; 
+	m_pestruct.m_dwpOptHeader->DataDirectory[1].VirtualAddress = 0;
+	m_pestruct.m_dwpOptHeader->DataDirectory[1].Size = 0;
 
 	m_pestruct.m_dwpOptHeader->DataDirectory[5].VirtualAddress = 0;
 	m_pestruct.m_dwpOptHeader->DataDirectory[5].Size = 0;
@@ -302,58 +204,40 @@ VOID ClearI_AT_ROC(Pestruct m_pestruct)
 
 }
 
-PVOID Copy2NewMem(PVOID m_dwpPeBase, INT m_dwCompressedDataSize, PVOID m_dwpComperedData, INT m_dwSizeOfDecMem, PVOID m_dwpDecMem)
-{
-	INT m_sizeofheaders = GetOptHeader(m_dwpPeBase)->SizeOfHeaders;
-
-	INT m_firstsecsize = GetAlignment(GetOptHeader(m_dwpPeBase)->FileAlignment, m_dwCompressedDataSize);
-
-	INT m_newpesize = m_sizeofheaders + m_firstsecsize;
-
-	PVOID m_ppeheader = UpdateHeader(m_dwpPeBase, m_firstsecsize);
-
-	PVOID m_pnewpe = malloc(m_newpesize + m_dwSizeOfDecMem);
-
-	if (m_pnewpe == NULL)
-	{
-		GetError("Copy2NewMem_malloc");
-		return NULL;
-	}
-
-	//copy Headers
-	memcpy_s(m_pnewpe, m_newpesize, m_ppeheader, m_sizeofheaders);
-
-	//copy compreed data
-	memcpy_s((PBYTE)m_pnewpe + m_sizeofheaders, m_newpesize, m_dwpComperedData, m_dwCompressedDataSize);
-
-	//copy Dec data
-	memcpy_s((PBYTE)m_pnewpe + m_firstsecsize, m_newpesize, m_dwpDecMem, m_dwSizeOfDecMem);
-
-	free(m_ppeheader);
-
-	return m_pnewpe;
-
-}
-//保存IDA和ROC
+//************************************
+// 函数名称: InitStuct
+// 函数说明: 保存IAT和ROC 供给之后解壳代码使用
+// 函数参数: Pestruct m_pestruct
+// 返 回 值: Pestruct
+// 创 建 人: Cray
+// 创建日期: 2020/10/26
+//************************************
 Pestruct InitStuct(Pestruct m_pestruct)
 {
 
 	m_pestruct.m_dwpOptHeader =
 		GetOptHeader(m_pestruct.mem_pe_base);   //保存内存中的PE
-	
+
 	m_pestruct.OEP = m_pestruct.m_dwpOptHeader->AddressOfEntryPoint;//保存原始入口点RVA
 
 	m_pestruct.IAT =
-		m_pestruct.m_dwpOptHeader->DataDirectory[1]; // IDA表项
+		m_pestruct.m_dwpOptHeader->DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT]; // IDA表项
 
 	m_pestruct.ROC =
-		m_pestruct.m_dwpOptHeader->DataDirectory[5]; // 重定位表项
+		m_pestruct.m_dwpOptHeader->DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC]; // 重定位表项
 
-
+	m_pestruct.oldImageBase = m_pestruct.m_dwpOptHeader->ImageBase;
 	return m_pestruct;
 }
 
-//导入packdll的代码段到内存
+
+//************************************
+// 函数名称: LoadPackdll
+// 函数说明: 导入packdll的代码段到内存
+// 返 回 值: StructUnPacker
+// 创 建 人: Cray
+// 创建日期: 2020/10/26
+//************************************
 StructUnPacker LoadPackdll()
 {
 	StructUnPacker spack;
@@ -373,7 +257,7 @@ StructUnPacker LoadPackdll()
 	memcpy_s(pNewDllTextSec, dwSecSize, pDllTextSec, dwSecSize);
 
 	//内存中是VA 要先减去基值,然后减去这个节的va，就能得到这个导出函数在节内的偏移
-	spack.unpackerStartfunc = unpackerStartfunc - (DWORD)PDllBase - (DWORD)pFristSec->VirtualAddress ;
+	spack.unpackerStartfunc = unpackerStartfunc - (DWORD)PDllBase - (DWORD)pFristSec->VirtualAddress;
 
 	spack.dwSecSize = dwSecSize;
 
@@ -383,25 +267,63 @@ StructUnPacker LoadPackdll()
 
 }
 
+//************************************
+// 函数名称: mprintf
+// 函数说明: 自己构造的printf，可通过debug选项控制输出
+// 函数参数: _In_z_ _Printf_format_string_ char const * const _Format
+// 函数参数: ...
+// 返 回 值: int __CRTDECL
+// 创 建 人: Cray
+// 创建日期: 2020/10/26
+//************************************
+int __CRTDECL mprintf(
+	_In_z_ _Printf_format_string_ char const* const _Format,
+	...)
+{
+	if (IsDebug)
+	{
+		int _Result;
+		va_list _ArgList;
+		__crt_va_start(_ArgList, _Format);
+		_Result = _vfprintf_l(stdout, _Format, NULL, _ArgList);
+		__crt_va_end(_ArgList);
+		return _Result;
+	}
+	else
+	{
+		return NULL;
+	}
+}
 
-VOID ConstructNewPE(Pestruct m_pestruct , StructUnPacker  m_StrucUnPacker)
+
+
+//************************************
+// 函数名称: ConstructNewPE
+// 函数说明: 在内存中构造新的PE，然后保存到文件中
+// 函数参数: Pestruct m_pestruct
+// 函数参数: StructUnPacker m_StrucUnPacker
+// 返 回 值: VOID
+// 创 建 人: Cray
+// 创建日期: 2020/10/26
+//************************************
+VOID ConstructNewPE(Pestruct m_pestruct, StructUnPacker  m_StrucUnPacker)
 {
 	INT NumberOfSections =
 		GetNTHeader(m_pestruct.mem_pe_base)->FileHeader.NumberOfSections;
 
 	PIMAGE_SECTION_HEADER pFirstSec =
 		IMAGE_FIRST_SECTION(GetNTHeader(m_pestruct.mem_pe_base));
-	
-	PIMAGE_SECTION_HEADER plastSec = 
+
+	PIMAGE_SECTION_HEADER plastSec =
 		(PCHAR)pFirstSec + (NumberOfSections - 1) * sizeof(IMAGE_SECTION_HEADER);
-	
+
 	INT PeFileSize = (PCHAR)plastSec->PointerToRawData + plastSec->SizeOfRawData;
-	
-	INT SumNewPESize = PeFileSize + GetAlignment(m_pestruct.m_dwpOptHeader->FileAlignment, m_StrucUnPacker.dwSecSize) ;
+
+	INT SumNewPESize = PeFileSize + GetAlignment(m_pestruct.m_dwpOptHeader->FileAlignment, m_StrucUnPacker.dwSecSize);
 
 
 	//增加最后一个节的信息
-	IMAGE_SECTION_HEADER pNewSec = {0,};
+	IMAGE_SECTION_HEADER pNewSec = { 0, };
 
 	memcpy_s(pNewSec.Name, 8, "packer!", 8);
 
@@ -431,7 +353,7 @@ VOID ConstructNewPE(Pestruct m_pestruct , StructUnPacker  m_StrucUnPacker)
 	PIMAGE_NT_HEADERS pNT = GetNTHeader(m_pestruct.mem_pe_base);
 
 	pNT->FileHeader.NumberOfSections = pNT->FileHeader.NumberOfSections + 1;
-	
+
 	pNT->OptionalHeader.AddressOfEntryPoint = pNewSec.VirtualAddress + m_StrucUnPacker.unpackerStartfunc;
 
 	PCHAR pNewPE = malloc(SumNewPESize);
@@ -443,7 +365,7 @@ VOID ConstructNewPE(Pestruct m_pestruct , StructUnPacker  m_StrucUnPacker)
 
 	memcpy_s(pNewPE + PeFileSize, SumNewPESize, m_StrucUnPacker.pNewDllTextSec, m_StrucUnPacker.dwSecSize);
 
-	FILE* fp = fopen("outFile.exe", "wb");
+	FILE* fp = fopen("Packed.exe", "wb");
 
 	fwrite(pNewPE, 1, SumNewPESize, fp);
 
@@ -453,68 +375,55 @@ VOID ConstructNewPE(Pestruct m_pestruct , StructUnPacker  m_StrucUnPacker)
 }
 
 
+//************************************
+// 函数名称: Run
+// 函数说明: 执行加壳代码的流程
+// 函数参数: LPWSTR sourceFile
+// 返 回 值: BOOL
+// 创 建 人: Cray
+// 创建日期: 2020/10/26
+//************************************
 BOOL Run(LPWSTR sourceFile)
 {
 
 	Pestruct m_pestruct;
 
-	//m_dwpPeBase = LoadFile2Mem(m_pSouceFile);
 	m_pestruct.mem_pe_base = LoadFile2Mem(sourceFile);
 
 	m_pestruct = InitStuct(m_pestruct);
 
-	EncryptExc(m_pestruct);
+	DecryptExc(m_pestruct);
 
 	ClearI_AT_ROC(m_pestruct);
 
-	//INT m_CompreDataSize = (INT)CalcCompreDataSize(m_dwpPeBase);
-
-	//m_dwpComperedData = CompreData(m_dwpPeBase, m_CompreDataSize);
-
-	//m_dwpSecheadersData = SaveSecheader(m_dwpPeBase);
-
-	//INT m_dwSizeOfDec = 0x400;
-
-	//PVOID m_dwpDecMem = malloc(m_dwSizeOfDec);//要修改为解壳的代码
-	
 	StructUnPacker m_StrucUnPacker = LoadPackdll();
 
 	ConstructNewPE(m_pestruct, m_StrucUnPacker);
-
-	//m_dwpNePeMem = Copy2NewMem(m_dwpPeBase, m_dwCompressedDataSize, m_dwpComperedData, m_dwSizeOfDec, m_dwpDecMem);
 
 	return TRUE;
 }
 
 
-int main()
+int main(int argc, char* argv[])
 {
 
 #ifdef _DEBUG
 	IsDebug = TRUE;
 #endif 
 
+	setlocale(LC_ALL, "chs");
+
+	printf("[*] 请输入要加壳程序:\n");
+	_getws(FilePath);
+
 	Run(FilePath);
+
+	printf("[*] 加壳完成！已加壳程序保存在Packer.exe工作目录下！\n[*] 按任意键退出...");
+
+	getchar();
+
 	return 0;
 
 }
 
 
-int __CRTDECL mprintf(
-	_In_z_ _Printf_format_string_ char const* const _Format,
-	...)
-{
-	if (IsDebug)
-	{
-		int _Result;
-		va_list _ArgList;
-		__crt_va_start(_ArgList, _Format);
-		_Result = _vfprintf_l(stdout, _Format, NULL, _ArgList);
-		__crt_va_end(_ArgList);
-		return _Result;
-	}
-	else
-	{
-		return NULL;
-	}
-}
